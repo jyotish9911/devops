7dcb41523b2b39678ccb862c1819642f856c904d
jyotish9911 <jyo9911@gmail.com>
2018-12-05 15:30:16 +0530

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,31 +1,6 @@
-<?php
-
-$name = $_GET['bid'];
-echo $name;
-
-$bookingid = $bid;
-//$bookingid = '1111';
-$bookingURL = "https://api.oyorooms.com/v2/wifi_management/validate_booking?booking_id=$bookingid";
+<form action="show.php" method="post" id="nameform">
+Booking id: <input type="text" name="bid"></br>
+<button type="submit" form="nameform" value="submit">Submit</button>
+</form>
 
 
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_HTTPHEADER, array('access_token:c0g5bUhKQjJBcW5DcUxRNm0yS246OWpxYmFxSExjQUpNYmtRWDlUWUQ='));
-curl_setopt($ch,CURLOPT_URL, $bookingURL);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,10);
-$result = curl_exec($ch);
-$response = array();
-if(curl_errno($ch))
-{
-        $response['status'] = -10;
-        $response['data'] = '';
-        $response['message'] = "Login Error Timeout. Please Try Again.";
-}
-curl_close($ch);
-//$response = json_decode($result);
-
-//print_r($result);
-//echo json_decode($result,JSON_PRETTY_PRINT);
-
-echo $result
-?>
