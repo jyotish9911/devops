c78c042e6e513f1eafddea65a93683cdbaaa7882
root <root@ip-172-31-26-163.ap-south-1.compute.internal>
2018-12-08 08:32:25 +0000

    modified nginx file

---
+++ app/index.php
@@ -0,0 +1,6 @@
+<form action="show.php" method="post" id="nameform">
+Booking id: <input type="text" name="bid"></br>
+<button type="submit" form="nameform" value="submit">Submit</button>
+</form>
+
+
